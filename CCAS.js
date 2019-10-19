6e84b14944476781aabf3a5aa4715201a71ecead
RB384 <raghavbansal.becse17@pec.edu.in>
2019-10-19 10:42:36 +0530

    Raghav 19-10-2019 10.42 am

--- CCAS.js
+++ CCAS.js
@@ -1,7 +1,6 @@
 var express=require("express");
 var bodyParser=require('body-parser');
 var session = require('express-session');
-
 var connection = require('./config');
 var app = express();
 
@@ -9,9 +8,12 @@ var app = express();
 app.use(express.static('Homepage'));
 app.use(express.static('Login'));
 app.use(express.static('Registration'));
+app.use(express.static('Student'));
+app.use(express.static('SocietyHead'));
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.set('view engine', 'ejs');
 
 app.get('/', function(req,res){
 	res.sendFile(__dirname + '/Homepage/index.html');
@@ -39,8 +41,29 @@ app.post("/postSignup", function (req, res) {
     	console.log("1 record inserted");
   });
        
-       
+      
 });
+
+app.get('/StudentFunc', function(req,res){
+	if(req.session.loggedin){
+		con.query("SELECT * FROM Student_Details where SID = ?", function (err, result, fields) {
+    	if (err) throw err;
+    	console.log(result);
+		res.render('Student',{data:res});
+});
+}
+});
+
+app.get('/SocietyHeadFunc', function(req,res){
+	/*if(req.session.loggedin){
+		req.session.destroy();
+	}*/
+	var name = 'hello';
+
+	res.render('SocietyHeadFunc',{name:name});
+});
+
+
 app.listen(3000, '0.0.0.0', function() {
 	console.log('Hosting started...');
 });
