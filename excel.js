8ab9eef5c92e34ca782bea0698c4d102ac09cab5
RB384 <raghavbansal.becse17@pec.edu.in>
2019-11-07 19:46:42 +0530

    Raghav Bansal Backup  07.11.2019 07.46 pm

--- excel.js
+++
@@ -1,5 +0,0 @@
-const XLSX =require('xlsx');
-const workbook = XLSX.readFile('UploadCertificationData.xlsx');
-const sheet_name_list = workbook.SheetNames;
-
-console.log(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[2]]))
